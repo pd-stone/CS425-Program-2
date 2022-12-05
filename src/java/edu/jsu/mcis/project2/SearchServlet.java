27c5b999a0107a83643c3556b57b28c1161c862a
PD Stone <71540038+pd-stone@users.noreply.github.com>
2022-12-04 19:43:40 -0600

    Add files via upload

---
+++ src/java/edu/jsu/mcis/project2/SearchServlet.java
@@ -0,0 +1,71 @@
+package edu.jsu.mcis.project2;
+
+import edu.jsu.mcis.project2.dao.*;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class SearchServlet extends HttpServlet {
+    
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try ( PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet SearchServlet</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet SearchServlet at " + request.getContextPath() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        DAOFactory daoFactory = null;
+
+        ServletContext context = request.getServletContext();
+        Bean projBean = (Bean)request.getSession().getAttribute("projBean");
+
+        if (context.getAttribute("daoFactory") == null) {
+            System.err.println("*** Creating new DAOFactory ...");
+            daoFactory = new DAOFactory();
+            context.setAttribute("daoFactory", daoFactory);
+        }
+        else {
+            daoFactory = (DAOFactory) context.getAttribute("daoFactory");
+        }
+        
+        try ( PrintWriter out = response.getWriter()) {
+            response.setContentType("application/json; charset=UTF-8");
+        
+            SearchDAO dao = daoFactory.getSearchDAO();  
+            out.println(dao.find(projBean.getParameters()));
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+        
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
